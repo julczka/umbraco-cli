fab073e999a7984a2213fa6bbc247d580c758759
Julia gru <julia.gru@gmail.com>
2021-09-17 14:04:04 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 #!/usr/bin/env node
 
-const init = require('./utils/init');
-const readline = require('readline');
-const changeCaseTo = require('./utils/change-case');
-const createFiles = require('./utils/create-files');
+const init = require("./utils/init");
+const readline = require("readline");
+const createFiles = require("./utils/create-files");
 const alert = require("cli-alerts");
 
-( () => {
-	init({clear: true});
-    let name = '';
+(() => {
+  init({ clear: true });
+  let name = "";
 
-    alert({ type: "info", msg: "Type in the name of your editor. You can use spaces." });
+  alert({
+    type: "info",
+    msg: "Type in the name of your editor. You can use spaces.",
+  });
 
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-    
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-      });
-      
-      rl.question('I wanna make a property editor called...  ', (answer) => {
-        // TODO: Log the answer in a database
-        name = answer;
-        console.log(`Scaffolding ${answer}...`);
-        
-         createFiles(name);
-         
-         rl.close();
-      });
+  rl.question("I wanna make a property editor called...  ", (answer) => {
+    name = answer;
+    console.log(`Scaffolding ${answer}...`);
 
-      
+    createFiles(name);
 
+    rl.close();
+  });
 })();
--- utils/files-data.js
+++ utils/files-data.js
@@ -1,46 +1,45 @@
-const changeCaseTo = require('./change-case');
-const toDotCase = require('to-dot-case')
+const changeCaseTo = require("./change-case");
+const toDotCase = require("to-dot-case");
 const dir = `${process.cwd()}/App_Plugins`;
-const propertyEditorPath = (name) => `${changeCaseTo.kebab(name)}`
-
-
+const propertyEditorPath = (name) => `${changeCaseTo.kebab(name)}`;
 
 const view = (name) => ({
-    name: `${changeCaseTo.kebab(name)}.html`,
-    content: `<div ng-controller="${changeCaseTo.camel(name)}Controller" class="${changeCaseTo.kebab(name)}">
+  name: `${changeCaseTo.kebab(name)}.html`,
+  content: `<div ng-controller="${changeCaseTo.camel(
+    name
+  )}Controller" class="${changeCaseTo.kebab(name)}">
     <input ng-model="model.value"></input>
     {{model.value}}
-</div>`
-
+</div>`,
 });
 
 const controller = (name) => ({
-
-    name: `${changeCaseTo.kebab(name)}.controller.js`,
-    content: `angular.module("umbraco")
+  name: `${changeCaseTo.kebab(name)}.controller.js`,
+  content: `angular.module("umbraco")
     .controller("${changeCaseTo.camel(name)}Controller",
     function () {
         alert("The ${changeCaseTo.camel(name)} controller has landed");
-    });`
-
+    });`,
 });
 
 const styles = (name) => ({
-    name: `${changeCaseTo.kebab(name)}.css`,
-    content: `.${changeCaseTo.kebab(name)} {
+  name: `${changeCaseTo.kebab(name)}.css`,
+  content: `.${changeCaseTo.kebab(name)} {
         color: red;
-    }`
-})
+    }`,
+});
 
 const manifest = (name) => ({
-    name: `package.manifest`,
-    content: `{
+  name: `package.manifest`,
+  content: `{
         "propertyEditors": [
           {
             "name": "${name}",
             "alias": "${toDotCase(name)}",
             "editor": {
-              "view": "~/App_Plugins/${propertyEditorPath(name)}/${view(name).name}"
+              "view": "~/App_Plugins/${propertyEditorPath(name)}/${
+    view(name).name
+  }"
             }
           }
         ],
@@ -50,7 +49,7 @@ const manifest = (name) => ({
         "css": [
             "~/App_Plugins/${propertyEditorPath(name)}/${styles(name).name}"
         ]
-      }`
-})
+      }`,
+});
 
 module.exports = [manifest, view, controller, styles];
