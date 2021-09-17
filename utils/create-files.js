14be0d917204a042c7b72f4a88b546069fc9bfc3
Julia gru <julia.gru@gmail.com>
2021-09-17 12:45:27 +0200

    fix create file

--- utils/create-files.js
+++ utils/create-files.js
@@ -4,20 +4,20 @@ const changeCaseTo = require("./change-case");
 const files = require("./files-data");
 
 const path = require("path");
+const dir = path.join(process.cwd(), "App_Plugins");
 
-    const dir = `${process.cwd()}/App_Plugins`;
-
-    if (!fs.existsSync(dir)) {
-        alert({type: 'error', msg: 'App_Plugins directory does not exist'})
-    } else {
-
-        console.log('Now we shall make some files!');
-        const propertyEditorPath = `${dir}/${changeCaseTo.kebab(name)}`
-        fs.mkdirSync(propertyEditorPath);
-        files.forEach(file => fs.writeFileSync(`${propertyEditorPath}/${file(name).name}`, file(name).content))
 module.exports = async (name) => {
-  const dir = path.join(process.cwd(), "App_Plugins");
-
-
-    }
-  } catch (error) {
+  if (!fs.existsSync(dir)) {
+    alert({ type: "error", msg: "App_Plugins directory does not exist" });
+  } else {
+    console.log("Now we shall make some files!");
+    const propertyEditorPath = `${dir}/${changeCaseTo.kebab(name)}`;
+    fs.mkdirSync(propertyEditorPath);
+    files.forEach((file) =>
+      fs.writeFileSync(
+        `${propertyEditorPath}/${file(name).name}`,
+        file(name).content
+      )
+    );
+  }
+};
